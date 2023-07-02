61b6e0074bf8deda98bfd99a3b903b6813772f42
jo-kl <72564384+jo-kl@users.noreply.github.com>
2023-07-02 15:06:16 +0200

    initial commit

---
+++ src/main/java/io/github/weblegacy/tiles/request/servlet/extractor/ParameterExtractor.java
@@ -0,0 +1,60 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.github.weblegacy.tiles.request.servlet.extractor;
+
+import java.util.Enumeration;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.apache.tiles.request.attribute.HasKeys;
+
+/**
+ * Extract parameters from the request.
+ * <p>Copied from Apache tiles-request-servlet 1.0.7 and adapted for Jakarta EE 9 </p>
+ *
+ * @version $Rev$ $Date$
+ */
+public class ParameterExtractor implements HasKeys<String> {
+
+    /**
+     * The servlet request.
+     */
+    private HttpServletRequest request;
+
+    /**
+     * Constructor.
+     *
+     * @param request The servlet request.
+     */
+    public ParameterExtractor(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return request.getParameterNames();
+    }
+
+    @Override
+    public String getValue(String key) {
+        return request.getParameter(key);
+    }
+}
